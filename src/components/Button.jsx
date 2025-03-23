12bb422bc164d3826e83a1814183c67ff2b2cdf7
Aditya Suresh Sharma <adityasharma0431@gmail.com>
2025-03-23 16:32:59 +0530

    Hero Section | Button Component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { HiMiniArrowLongRight } from "react-icons/hi2";
 
-const Button = () => {
+const Button = ({
+  text,
+  onClick,
+  className = "",
+  type = "button",
+  variant = "primary", // 'primary' or 'secondary'
+  link = "",
+  target = "_self",
+  showIcon = false, // Toggle arrow icon for secondary buttons
+}) => {
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    if (onClick) {
+      onClick(e);
+    }
+    if (link) {
+      if (link.startsWith("http")) {
+        window.open(link, target);
+      } else {
+        navigate(link);
+      }
+    }
+  };
+
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      className={`btn-comp ${variant}-btn ${className}`}
+      onClick={handleClick}
+      type={type}
+    >
+      {text}
+      {showIcon && variant === "secondary" && <HiMiniArrowLongRight />}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "./style.css";
+import Button from "../../components/Button";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="hero-content">
+        <div className="homepage" id="home">
+          <h1>
+            BUILT FOR PROTECTION,
+            <br />
+            DRIVEN BY SAFETY,
+            <br />
+            CRAFTED WITH EXCELLENCE.
+          </h1>
+          <div className="hero-buttons">
+            <Button text="Explore" variant="primary" link="/shop" />
+            <Button
+              text="Contact"
+              variant="secondary"
+              link="/contact"
+              showIcon={true}
+            />
+          </div>
+        </div>
+      </div>
+      
+    </>
+  );
 };
 
 export default Home;
